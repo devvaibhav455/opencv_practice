04adaf10fc5d7359db5d9ac305340c00cee7576a
Dev Vaibhav <vaibhav.d@northeastern.edu>
2022-12-21 17:29:46 -0500

    OpenCV read image from file successful

---
+++ build/CMakeFiles/my_opencv_package.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_opencv_package.dir/src/read_image.cpp.o"
+  "CMakeFiles/my_opencv_package.dir/src/read_image.cpp.o.d"
+  "bin/my_opencv_package"
+  "bin/my_opencv_package.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_opencv_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
